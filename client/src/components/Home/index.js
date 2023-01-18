1f7d074e283bfbf59c7f672c71ce4d43900d659c
joesakkab <82406960+joesakkab@users.noreply.github.com>
2023-01-18 17:37:39 -0500

    Update index.js
    Change

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci245!
+                Welcome to MSci245! Joe has made a change!
               </React.Fragment>
             ) : (
               <React.Fragment>
