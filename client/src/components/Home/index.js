5f5f72785d8a643c5f050d51dfa1a755a3bea42f
twarep <ptwareck@uwaterloo.ca>
2023-01-19 11:52:39 -0500

    changed MSCI 245 to MSCI 342

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci245!
+                Welcome to our MSCI 343 Project!
               </React.Fragment>
             ) : (
               <React.Fragment>
