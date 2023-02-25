77dab953e7f5fa4e2a88f68acf65ef3142904761
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2023-02-25 12:21:15 -0500

    added code for handling auth and login in the backend

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 let mysql = require('mysql');
 let config = require('./config.js');
 const fetch = require('node-fetch');
+
+// Authentication services
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "" //Paste databaseURL from firebaseConfig here
+});
+
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -13,20 +24,57 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+// Auth middleware
+const auth = async (req, res, next) => {
+	const token = req.headers.authorization.split(" ")[1];
+	if (!token)
+		return res.status(403).send({ auth: false, message: "No token provided." });
+	await jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+		if (err)
+			return res
+				.status(500)
+				.send({ auth: false, message: "Failed to authenticate token." });
+		// if everything good, save to request for use in other routes
+		req.userID = decoded.userID;
+		next();
+	});
+};
 
 app.get('/api/ping', (req, res) => {
 	res.send({ express: "pong" });
 });
 
-app.post('/api/loadUserSettings', (req, res) => {
+app.post("/login", (req, res) => {
+	const token = req.body.token;
+	// idToken comes from the client app
+	admin
+		.auth()
+		.verifyIdToken(token)
+		.then(function (decodedToken) {
+			let uid = decodedToken.uid;
+			const token = jwt.sign({ userID: uid, sub: uid }, process.env.JWT_KEY, {
+				expiresIn: 86400 // expires in 24 hours
+			});
+			//console.log(token);
+			res.status(200).send({ auth: true, token: token });
 
+		})
+		.catch(function (error) {
+			// error on verification
+			console.log(error);
+			res.status(404).send("No user found");
+		});
+});
+
+app.post('/api/loadUserSettings', (req, res) => {
 	let connection = mysql.createConnection(config);
 	let userID = req.body.userID;
+	console.log(userID);
 
-	let sql = `SELECT mode FROM user WHERE userID = ?`;
+	let sql = `SELECT mode FROM user WHERE userID = 1`;
 	console.log(sql);
-	let data = [userID];
-	console.log(data);
+	let data = [];
+	//console.log(data);
 
 	connection.query(sql, data, (error, results, fields) => {
 		if (error) {
@@ -41,6 +89,25 @@ app.post('/api/loadUserSettings', (req, res) => {
 });
 
 
+
+
+
+
+app.post('/api/login', (req, res) => {
+	res.send({ express: "pong" });
+});
+
+app.post('/api/signup', (req, res) => {
+	res.send({ express: "pong" });
+});
+
+app.get('/api/ping', (req, res) => {
+	res.send({ express: "pong" });
+});
+
+app.get('/api/ping', (req, res) => {
+	res.send({ express: "pong" });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
 //app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
