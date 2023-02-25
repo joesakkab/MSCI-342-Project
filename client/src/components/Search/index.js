bcc638db5d508db10ce14c42d6cddd68403071ca
joesakkab <jsakkab@uwaterloo.ca>
2023-02-25 15:38:05 -0500

    Added one more json object for test and displayed the listing info in the search page

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography, Button, TextField } from '@material-ui/core';
+import { Paper, Typography, TextField, Button } from '@material-ui/core';
+import { Card, CardActionArea, CardContent } from '@material-ui/core'
 import NavBar from '../NavigationBar';
+import data from "./sample-data.json"
+import Rating from '@material-ui/lab/Rating'
+import PrivateRoute from '../Navigation/PrivateRoute';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   test: {
     padding: theme.spacing(5),
     // display: 'flex', 
-    // marginTop: theme.spacing(3), 
+    // marginTop: theme.spacing(2), 
     // borderRadius: 50
   },
   title: {
@@ -47,7 +51,11 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#2196f3', // Set the button background color to match the background color of the page
     color: '#fff', // Set the button text color to white
   },
+  listing: {
+    padding: theme.spacing(1)
+  }
 }));
+
 
 
 function Search() {
@@ -57,7 +65,34 @@ function Search() {
         <NavBar />
         <div className={classes.test}>
             <TextField  style={{ width:1000 }} placeholder="Search for a service ..." variant="outlined"  id="search"/>
+            <Button> Search </Button>
         </div>
+
+        
+            {data.map((obj) => (
+                <div className={classes.listing}>
+                    <Card style={{width: 1000}}>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography id="title" variant="h4">
+                                    {obj.first + " " + obj.last + " - " + obj['serviceType']}
+                                </Typography>
+                                <Typography id="description">
+                                    {obj.description}
+                                </Typography>
+                                <Typography sx={{fontWeight: 'bold'}} id="location">
+                                    {obj["primaryLocation"]}
+                                </Typography>
+                                <Rating name="half-rating-read" defaultValue={obj.rating} precision={0.1} readOnly />
+                                <Typography>
+                                    {obj.rating + " / 5.0"}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </div>
+            ))}
+        
       
     </div>
   );
