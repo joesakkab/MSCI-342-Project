926aa1f0b0e343036a9532b1794518b634d49171
Kishoesanjay Rajesh <kishoesanjay@Kishoesanjays-MacBook-Air.local>
2023-03-03 01:27:05 -0500

    sign-up

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,54 +1,90 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Fragment, useState, useSyncExternalStore } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Typography,
   Paper,
   TextField,
   Button,
-} from '@material-ui/core';
+  Checkbox,
+  FormControlLabel,
+} from "@material-ui/core";
+import validator from "validator";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
-    backgroundColor: '#2196f3',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'DM Sans, sans-serif',
+    height: "100vh",
+    backgroundColor: "#2196f3",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "DM Sans, sans-serif",
   },
   title: {
     marginBottom: theme.spacing(2),
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
   },
   description: {
     marginBottom: theme.spacing(4),
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
     maxWidth: 600,
-    margin: '0 auto',
+    margin: "0 auto",
   },
   paper: {
     padding: theme.spacing(3),
     maxWidth: 400,
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: theme.spacing(4),
   },
   button: {
-    width: '100%',
+    width: "100%",
     margin: theme.spacing(1),
-    backgroundColor: '#2196f3',
-    color: '#fff',
+    backgroundColor: "#2196f3",
+    color: "#fff",
   },
 }));
 
 function SignUp() {
   const classes = useStyles();
+
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+  const [passwordMatch, setPasswordMatch] = useState();
+  const [validEmail, setValidEmail] = useState();
+  const [additionalInfo, setAdditionalInfo] = useState("");
+  const [fName, setFName] = useState("");
+  const [LName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [location, setLocation] = useState("");
+  const [serviceType, setServiceType] = useState("");
+
+  const handleCheckboxChange = (event) => {
+    setShowAdditionalInfo(event.target.checked);
+  };
+
+  const handleAdditionalInfoChange = (event) => {
+    setAdditionalInfo(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    let submitUser = {
+      firstName: fName,
+      lastName: LName,
+      email: email,
+      password: password,
+      location: location,
+      serviceType: serviceType
+    }
+
+    console.log(submitUser);
+  }
 
   return (
     <div className={classes.root}>
@@ -58,8 +94,41 @@ function SignUp() {
       <Typography variant="body1" className={classes.description}>
         Fill in the form below to create an account.
       </Typography>
-      <Paper elevation={3} className={classes.paper}>
+      <Paper elevation={5} className={classes.paper}>
         <form noValidate autoComplete="off">
+          <TextField
+            label="First Name"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoFocus
+            value={fName}
+            onChange={(fName) => setFName(fName.target.value)}
+            inputProps={{ maxLength: 30 }}
+          />
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoFocus
+            value={LName}
+            onChange={(LName) => setLName(LName.target.value)}
+            inputProps={{ maxLength: 30 }}
+          />
+          <TextField
+            label="Primary Location"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoFocus
+            value={location}
+            onChange={(location) => setLocation(location.target.value)}
+            inputProps={{ maxLength: 30 }}
+          />
           <TextField
             label="Email"
             variant="outlined"
@@ -67,6 +136,9 @@ function SignUp() {
             required
             fullWidth
             autoFocus
+            value={email}
+            onChange={(email) => setEmail(email.target.value)}
+            inputProps={{ maxLength: 30 }}
           />
           <TextField
             label="Password"
@@ -75,6 +147,9 @@ function SignUp() {
             type="password"
             required
             fullWidth
+            value={password}
+            onChange={(password) => setPassword(password.target.value)}
+            inputProps={{ maxLength: 30 }}
           />
           <TextField
             label="Confirm Password"
@@ -83,11 +158,57 @@ function SignUp() {
             type="password"
             required
             fullWidth
+            value={confPassword}
+            onChange={(confPassword) =>
+              setConfPassword(confPassword.target.value)
+            }
+            inputProps={{ maxLength: 30 }}
           />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={showAdditionalInfo}
+                onChange={handleCheckboxChange}
+                name="showAdditionalInfo"
+                color="primary"
+              />
+            }
+            label="You are a Service Provider"
+          />
+          {showAdditionalInfo && (
+            <Fragment>
+              <TextField
+                label="Description"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                autoFocus
+                value={additionalInfo}
+                onChange={handleAdditionalInfoChange}
+                multiline
+                rows={4}
+                inputProps={{ maxLength: 500 }}
+              />
+              <TextField
+                label="Your Service Type"
+                variant="outlined"
+                margin="normal"
+                type="password"
+                required
+                fullWidth
+                value={serviceType}
+                onChange={(serviceType) =>
+                  setServiceType(serviceType.target.value)
+                }
+                inputProps={{ maxLength: 30 }}
+              />
+            </Fragment>
+          )}
           <Button
             variant="contained"
             color="primary"
             className={classes.button}
+            onClick = {handleSubmit}
           >
             Sign Up
           </Button>
