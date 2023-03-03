4786204190406800ec6b87e510063b37871c5aaf
krajesh00 <kishraj1807@gmail.com>
2023-03-03 16:44:23 -0500

    some more

---
+++ client/src/components/SignIn/SignIn.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SignIn from './';
+//Kishoresanjay Rajesh Unit Test
+
+describe('SignIn component', () => {
+  it('should render a SignIn button', () => {
+    render(<SignIn />);
+    const signInButton = screen.getByText('SignIn');
+    expect(signInButton).toBeInTheDocument();
+  });
+});
