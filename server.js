e0e6c8f6940819c0cf1a9b72dea66ced06b1e2dd
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2023-03-02 20:46:05 -0500

    Added sign up endpoint

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 let mysql = require('mysql');
 let config = require('./config.js');
 const fetch = require('node-fetch');
-
-// Authentication services
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "" //Paste databaseURL from firebaseConfig here
-});
-
+const bcrypt = require("bcrypt");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
 const { response } = require('express');
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -24,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// Auth middleware
+// Auth middleware for JWT authenticated endpoints
 const auth = async (req, res, next) => {
 	const token = req.headers.authorization.split(" ")[1];
 	if (!token)
@@ -44,70 +36,53 @@ app.get('/api/ping', (req, res) => {
 	res.send({ express: "pong" });
 });
 
-app.post("/login", (req, res) => {
-	const token = req.body.token;
-	// idToken comes from the client app
-	admin
-		.auth()
-		.verifyIdToken(token)
-		.then(function (decodedToken) {
-			let uid = decodedToken.uid;
-			const token = jwt.sign({ userID: uid, sub: uid }, process.env.JWT_KEY, {
-				expiresIn: 86400 // expires in 24 hours
-			});
-			//console.log(token);
-			res.status(200).send({ auth: true, token: token });
-
-		})
-		.catch(function (error) {
-			// error on verification
-			console.log(error);
-			res.status(404).send("No user found");
-		});
-});
-
-app.post('/api/loadUserSettings', (req, res) => {
+app.post("/signup", async (req, res) => {
 	let connection = mysql.createConnection(config);
-	let userID = req.body.userID;
-	console.log(userID);
 
-	let sql = `SELECT mode FROM user WHERE userID = 1`;
-	console.log(sql);
-	let data = [];
-	//console.log(data);
+	const email = req.body.email;
+	const pwd = req.body.password;
+	const first = req.body.first;
 
-	connection.query(sql, data, (error, results, fields) => {
-		if (error) {
-			return console.error(error.message);
+	const pwdHashed = await bcrypt.hash(pwd, 10);
+
+	connection.query(
+		`SELECT * FROM user WHERE email = ${email}`, 
+		[], 
+		(error, results, fields) => {
+			if (error) {
+				return console.error(error.message);
+			}
+
+			if (results.length > 0) {
+				res.status(403).send({ error: "User already exists!" });
+				return // User already exists
+			} else {
+				let sql = `INSERT INTO user (email, password, first, last) VALUES (?, ?, ?, ?)`;
+				console.log(sql);
+				let data = [email, pwdHashed, first, last];
+				console.log(data);
+
+				connection.query(
+
+					sql, 
+					data, 
+					(error, results, fields) => {
+						if (error) {
+							return console.error(error.message);
+						}
+				
+						let string = JSON.stringify(results);
+						//let obj = JSON.parse(string);
+						res.send({ express: string });
+					}
+				);
+			}
 		}
-
-		let string = JSON.stringify(results);
-		//let obj = JSON.parse(string);
-		res.send({ express: string });
-	});
+	);
 	connection.end();
 });
 
 
-
-
-
-
-app.post('/api/login', (req, res) => {
-	res.send({ express: "pong" });
-});
-
-app.post('/api/signup', (req, res) => {
-	res.send({ express: "pong" });
-});
-
-app.get('/api/ping', (req, res) => {
-	res.send({ express: "pong" });
-});
-
-app.get('/api/ping', (req, res) => {
-	res.send({ express: "pong" });
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
 //app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
