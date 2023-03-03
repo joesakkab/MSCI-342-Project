b8439da1468588eb761f8dbba61349a4c8bc29bc
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2023-03-03 01:04:58 -0500

    Added get profile endpoint

--- server.js
+++ server.js
@@ -115,5 +115,27 @@ app.post("/login", async (req, res) => {
 	connection.end();
 });
 
+app.post('/api/getprofile', (req, res) => {
+	let connection = mysql.createConnection(config);
+
+	let userID = req.body.userID;
+
+	let sql = `SELECT * FROM user WHERE userID = '?'`;
+	console.log(sql);
+	let data = [userID];
+	console.log(data);
+
+	connection.query(sql, data, (error, results, fields) => {
+		if (error) {
+			return console.error(error.message);
+		}
+
+		let string = JSON.stringify(results);
+		let obj = JSON.parse(string);
+		res.send({ results: obj });
+	});
+	connection.end();
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
 //app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
