f5776c091968aab8ddad9d8a4f96f54e4d9d59a5
joesakkab <jsakkab@uwaterloo.ca>
2023-03-03 17:48:26 -0500

    landing page jest file

--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Landing from './index';
+import { render, screen, component } from '@testing-library/react'
 
 it('Landing Page Renders', () => {
   const tree = renderer
     .create(<Landing />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+
 });
+
+it('should contain a sign up button', () => {
+  render(<Landing />);
+  const signup = screen.getByText('Sign Up');
+  expect(signup);
+})
+
+it('should contain a login button', () => {
+  render(<Landing />);
+  const login = screen.getByText('Login');
+  expect(login);
+
+}); 
