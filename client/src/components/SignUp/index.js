f6a0d318bb82adc4b8a04de3835c50467e861da1
joesakkab <jsakkab@uwaterloo.ca>
2023-03-14 01:37:05 -0400

    sign up button takes user to search page

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -10,6 +10,8 @@ import {
 } from "@material-ui/core";
 import validator from "validator";
 import * as FIELDS from '../../constants/serviceProviderConst';
+import history from "../Navigation/history";
+import * as ROUTES from "../../constants/routes";
 // import * as FIELDS from '../../constants/customerConst';
 
 const useStyles = makeStyles((theme) => ({
@@ -121,6 +123,7 @@ function SignUp() {
       }
       console.log(submitUser)
       addSignup(submitUser)
+      history.push(ROUTES.SEARCH);
 
     } else if (password !== confPassword) {
       alert("Passwords do not match please re-enter!")
